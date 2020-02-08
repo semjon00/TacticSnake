9d9b365110c3322452b43320198daa5217d44c12
Semjon Kravtsenko <semjon.00@gmail.com>
2020-02-08 19:44:34 +0200

    A draft

--- engine.cpp
+++ engine.cpp
@@ -204,6 +204,7 @@ char waitKey()
     return c;
 }
 
+// Easily reversible and fast int hash function
 unsigned int hash(unsigned int x) {
     x = ((x >> 16) ^ x) * 0x45d9f3b;
     x = ((x >> 16) ^ x) * 0x45d9f3b;
